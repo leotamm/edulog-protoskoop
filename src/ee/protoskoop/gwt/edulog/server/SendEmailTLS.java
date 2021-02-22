9ffdc51189eefd8af97652d73a672a47dc1ca0e1
Leo Tamm <leo.tamm@gmail.com>
2021-02-22 12:45:07 +0200

    sending emails

---
+++ src/ee/protoskoop/gwt/edulog/server/SendEmailTLS.java
@@ -0,0 +1,56 @@
+package ee.protoskoop.gwt.edulog.server;
+
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import ee.protoskoop.gwt.edulog.shared.User;
+
+
+public class SendEmailTLS {
+
+	public static boolean sendNewPassword(User user) {
+
+		boolean reply = false;
+
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "587");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.starttls.enable", "true");
+
+		Session session = Session.getInstance(prop,
+				new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(Configuration.EMAIL_USER, Configuration.EMAIL_PASS);
+			}
+		});
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(Configuration.EMAIL_USER));
+			message.setRecipients(
+					Message.RecipientType.TO,
+					InternetAddress.parse(user.getEmail())
+					);
+			message.setSubject("Resetting Edulog password");
+			message.setText("Dear Edulog user,"
+					+ "\n\n your new password is: " + user.getPassword());
+
+			Transport.send(message);
+
+			reply = true;
+
+		} catch (MessagingException e) {
+			e.printStackTrace();
+
+		}
+
+		return reply;
+
+	}
+
+}
